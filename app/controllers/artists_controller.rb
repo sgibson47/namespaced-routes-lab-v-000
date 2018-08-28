e3eaae3e236f4744fe6a54e2217dc7f577e676a1
sgibson47 <sgibson47@gmail.com>
2018-08-28 13:09:25 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,11 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    @preference = Preference.first
+    if @preference && @preference.artist_sort_order
+      @artists = Artist.order(name: @preference.artist_sort_order)
+    else
+      @artists = Artist.all
+    end
   end
 
   def show
@@ -8,7 +13,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    @preference = Preference.first
+    if @preference && !@preference.allow_create_artists
+      redirect_to artists_path
+    else
+      @artist = Artist.new
+    end
   end
 
   def create
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,17 @@
 class SongsController < ApplicationController
   def index
+    @preference = Preference.first
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
+      elsif @preference && @preference.song_sort_order
+        @songs = Song.order(title: @preference.song_sort_order)
       else
         @songs = @artist.songs
       end
+    elsif @preference && @preference.song_sort_order
+      @songs = Song.order(title: @preference.song_sort_order)
     else
       @songs = Song.all
     end
@@ -25,7 +30,12 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    @preference = Preference.first
+    if @preference && !@preference.allow_create_songs
+      redirect_to songs_path 
+    else
+      @song = Song.new
+    end
   end
 
   def create
